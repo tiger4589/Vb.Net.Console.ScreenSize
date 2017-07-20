ac740a5a5166d661cf210b2f6fe54c49c9b2df00
Paul <paulkaram1989@hotmail.com>
2017-07-20 22:54:15 +0300

    Initial Commit

---
+++ ConsoleScreenSize/ConsoleScreenSize/GetScreenSize.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Windows.Forms
+
+Module GetScreenSize
+
+    Sub Main()
+        Dim rectangle  = Screen.PrimaryScreen.Bounds
+        Dim workingArea  = Screen.PrimaryScreen.WorkingArea
+
+        Console.WriteLine("Bounds: Height: " & rectangle.Height & ", Width: " & rectangle.Width)
+        Console.WriteLine("WorkingArea: Height: " & workingArea.Height & ", Width: " & workingArea.Width)
+
+        Console.ReadKey()
+    End Sub
+
+End Module
